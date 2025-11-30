8c0c45c43722bf45bcfc2b42071ecd305f30c4e3
Girija Nandan Sahoo <girijanandansahoo3@gmail.com>
2025-12-01 04:40:22 +0530

    Updated main.py to remove comments and added .gitignore and REAME.MD

--- main.py
+++ main.py
@@ -8,15 +8,9 @@ from src.transformation.story_2 import analyze_for_story2
 
 
 def main():
-    import os
-    # print(os.environ.get("HADOOP_HOME"))
-    # print(os.environ.get("PATH"))
-
     logger = create_logger(__name__, logging.INFO)
     spark = create_spark_session('swiss_re_job')
     logger.info(f"spark session : {spark.sparkContext.applicationId}")
-
-    # print(spark.sparkContext._jvm.org.apache.hadoop.util.VersionInfo.getVersion())
 
     factory = ReaderFactory(spark)
     claims_data_path = './data/claims.parquet'
@@ -27,19 +21,12 @@ def main():
 
         # Read claims
         claims_df = parquet_reader.read_file(claims_data_path)
-        # claims_df.show(10, truncate=False)
 
         # Read customers
         customers_df = parquet_reader.read_file(customers_data_path)
-        # customers_df.show(10, truncate=False)
 
         # Read products
         products_df = parquet_reader.read_file(products_data_path)
-        # products_df.show(10, truncate=False)
-
-        # print(f"claims: {claims_df.columns}")
-        # print(f"customers: {customers_df.columns}")
-        # print(f"products: {products_df.columns}")
 
         # Result for Story1: Provide insights into claim behavior for our Health and Life products across various customer tiers and geographic locations.
         story_1_df = analyze_for_story1(claims_df, products_df, customers_df)
@@ -47,9 +34,6 @@ def main():
 
         story_2_df = analyze_for_story2(claims_df, products_df)
         story_2_df.coalesce(1).write.option('header','true').format("csv").mode('overwrite').save('./output/story_2/')
-        # story_2_df.show(1000)
-
-        #TODO-- do I need to write this data to some place?
 
     except Exception as ex:
         traceback_message = traceback.format_exc()
